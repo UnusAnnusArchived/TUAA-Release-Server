98d93903cc3550f4dd2dafd176fcc4fb7ea84d70
ScribbleNerd <contact@scribblenerd.com>
2023-04-08 23:03:15 -0500

    Add endpoints template

---
+++ src/endpoints.template.ts
@@ -0,0 +1,4 @@
+export const api = "https://unusann.us/api";
+export const siteRoot = "http://release.unusann.us";
+export const appDl = "https://app-dl.unusann.us";
+export const pocketbase = "https://db.unusann.us";
